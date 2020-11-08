282d5a5e8ccc1d9b29e6cd09d537a8d5990a2169
JINOOK JUNG <jinook929@gmail.com>
2020-11-08 02:00:40 +0000

    Automatically backed up by Learn

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-
+  
 });
 
-  $("#text").append("this is so freaking cool.");
-  $('#text').css("background-color", "pink");
-  $('#text').css("text-align", "center");
+$("#text").append("this is so freaking cool.");
+$('#text').css("background-color", "pink");
+$('#text').css("text-align", "center");
