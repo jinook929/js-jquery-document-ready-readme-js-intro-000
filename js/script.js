d5b4ebf99b6ddd05ee38ac8f3a61bc686544b7a4
JINOOK JUNG <jinook929@gmail.com>
2020-11-08 01:55:58 +0000

    Automatically backed up by Learn

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
+
+
+$("#text").append("this is so freaking cool.");
+
 $(document).ready(function() {
     // code to be executed goes here
     alert("executed!!!")
 });
-
-$("#text").append("this is so freaking cool.");
